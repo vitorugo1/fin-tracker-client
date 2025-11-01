ab161f49f14ed07b6c73227441afdf8bea10f3bc
vitorugo1 <hvitor79@gmail.com>
2025-11-01 15:33:00 -0300

    StocksCardProps

--- components/StocksCard/index.tsx
+++ components/StocksCard/index.tsx
@@ -6,14 +6,33 @@ import {
   CardTitle,
 } from '../ui/card';
 
-export function StocksCard() {
+type StocksCardProps = {
+  stocksName: string;
+  stocksTicker: string;
+  currentPrice: number;
+  dayChange: number;
+};
+
+export function StocksCard({
+  stocksName,
+  stocksTicker,
+  currentPrice,
+  dayChange,
+}: StocksCardProps) {
   return (
-    <Card>
+    <Card className='bg-zinc-900 border-zinc-800'>
       <CardHeader>
-        <CardTitle>Test</CardTitle>
-        <CardDescription>testing</CardDescription>
+        <CardTitle className='text-zinc-50'>{stocksName}</CardTitle>
+        <CardDescription className='text-zinc-400'>
+          {stocksTicker}
+        </CardDescription>
       </CardHeader>
-      <CardContent>test</CardContent>
+      <CardContent>
+        <div className='flex gap-4'>
+          <p className='text-zinc-50'>{currentPrice}</p>
+          <p className='text-green-500'>{dayChange}</p>
+        </div>
+      </CardContent>
     </Card>
   );
 }
